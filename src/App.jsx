01943aa3991c6e116a4c818f1aac9a2f734c2fd0
Bryan <bryanlug2014@gmail.com>
2024-12-14 17:58:03 -0500

    C

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import Login from './Frontend/Login/Index'
 import Usuarios from './Frontend/Admin/Users/Index'
 import Facturacion from './Frontend/Admin/Facturacion/Index'
+import Factura from './Frontend/Admin/Facturacion/Containers/Factura';
 
 
 function App() {
   return (
     <div className="App">
-     <Login/>
-        {/*<Usuarios/>*/}
-   {/*           
+     
+        
+            
    
-   <Facturacion/>
-   */}   
+   <Factura/>
+    
     </div>
   );
 }
